f1bb3c973fd54ede7c5d374db0c8048f2814474a
vinrata <vinnycamma@gmail.com>
2021-09-09 23:16:53 -0500

    cleaned up code again

---
+++ src/org/launchcode/java/studios/restaurantmenu/Main.java
@@ -0,0 +1,11 @@
+package org.launchcode.java.studios.restaurantmenu;
+
+public class Main {
+    public  static void main(String[] args){
+        MenuItem salad = new MenuItem();
+        salad.setDescription("Salad with fresh garden leaves.");
+        salad.setPrice(7.99);
+        salad.setCategory("Appetizer");
+        salad.setNew(true);
+    }
+}
